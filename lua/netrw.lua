15006a923a376ce2e621bd27075fc250f2bdc1c2
kolyamba2105 <haravetskyy@gmail.com>
2022-10-25 14:48:24 +0300

    netrw - Set initial window size

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -2,6 +2,7 @@ local map = require('mappings')
 
 vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = '.DS_Store'
+vim.g.netrw_winsize = 20
 
 map('n', '<C-n>', '<cmd>Lexplore<cr>')
 map('n', '<leader>e', '<cmd>Ex<cr>')
