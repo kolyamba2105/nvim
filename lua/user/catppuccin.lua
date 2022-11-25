658f3452389e10850ecc91139163af791174b788
kolyamba2105 <haravetskyy@gmail.com>
2022-11-26 00:11:34 +0200

    catppuccin - Remove italics

--- lua/user/catppuccin.lua
+++ lua/user/catppuccin.lua
@@ -3,3 +3,9 @@ require('catppuccin').setup {
 }
 
 vim.cmd [[ colorscheme catppuccin ]]
+
+vim.api.nvim_set_hl(0, "@conditional", { fg = "#cba6f7" })
+vim.api.nvim_set_hl(0, "@namespace", { fg = "#89b4fa" })
+vim.api.nvim_set_hl(0, "@parameter", { fg = "#eba0ac" })
+vim.api.nvim_set_hl(0, "@tag.attribute", { fg = "#94e2d5" })
+vim.api.nvim_set_hl(0, "@text.emphasis", { fg = "#eba0ac" })
