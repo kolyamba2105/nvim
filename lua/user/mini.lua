ffd730a1bbf724741e682cc469f93fdd600bc5a8
kolyamba2105 <haravetskyy@gmail.com>
2022-11-26 00:12:28 +0200

    mini - Set cursor word highlight group

--- lua/user/mini.lua
+++ lua/user/mini.lua
@@ -3,3 +3,6 @@ require('mini.comment').setup()
 require('mini.cursorword').setup({ delay = 500 })
 require('mini.misc').setup()
 require('mini.pairs').setup()
+
+vim.api.nvim_set_hl(0, "MiniCursorword", { bg = "#45475a", fg = "#a6e3a1" })
+vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { bg = "#45475a", fg = "#a6e3a1" })
