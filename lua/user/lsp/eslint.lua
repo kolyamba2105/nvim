9fc948bcdbdd24302c6f2e217e6859df41c0726f
kolyamba2105 <haravetskyy@gmail.com>
2022-12-08 13:18:53 +0200

    LSP - Add mapping for lint fix

--- lua/user/lsp/eslint.lua
+++ lua/user/lsp/eslint.lua
@@ -4,10 +4,6 @@ return {
   capabilities = common.capabilities,
   on_attach = function(client, bufnr)
     common.on_attach(client, bufnr)
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      callback = function() vim.cmd [[ EslintFixAll ]] end,
-      group = vim.api.nvim_create_augroup('LintFix', { clear = true }),
-    })
+    common.buf_set_keymap('<C-f>', function() vim.cmd('EslintFixAll') end)
   end,
 }
