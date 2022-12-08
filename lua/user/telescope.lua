d9c80fc9c001c06cd8e93ed5ab42e0bcb661fa0a
kolyamba2105 <haravetskyy@gmail.com>
2022-12-08 14:20:47 +0200

    telescope - Open file browser from buffer directory

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -4,22 +4,30 @@ local map = require('mappings')
 local opts = { silent = true }
 
 local function get_picker(picker)
-  return function()
-    return require('telescope.builtin')[picker](require('telescope.themes').get_ivy({}))
+  return function(options)
+    return require('telescope.builtin')[picker](require('telescope.themes').get_ivy(options))
   end
 end
 
+local enter_normal_mode = {
+  on_complete = { function() vim.cmd('stopinsert') end }
+}
+
+local function get_picker_normal(picker)
+  return function() get_picker(picker)(enter_normal_mode) end
+end
+
 map('n', '<leader>ta', require('telescope.builtin').resume, opts)
-map('n', '<leader>tb', get_picker('buffers'), opts)
+map('n', '<leader>tb', get_picker_normal('buffers'), opts)
 map('n', '<leader>tc', get_picker('command_history'), opts)
 map('n', '<leader>tf', get_picker('find_files'), opts)
 map('n', '<leader>tg', get_picker('live_grep'), opts)
 map('n', '<leader>th', get_picker('help_tags'), opts)
-map('n', '<leader>tj', get_picker('jumplist'), opts)
+map('n', '<leader>tj', get_picker_normal('jumplist'), opts)
 map('n', '<leader>tm', get_picker('marks'), opts)
-map('n', '<leader>tr', get_picker('grep_string'), opts)
+map('n', '<leader>tr', get_picker_normal('grep_string'), opts)
 map('n', '<leader>ts', get_picker('search_history'), opts)
-map('n', '<leader>tq', get_picker('quickfix'), opts)
+map('n', '<leader>tq', get_picker_normal('quickfix'), opts)
 
 -- Git
 map('n', '<leader>gc', get_picker('git_commits'), opts)
@@ -28,12 +36,18 @@ map('n', '<leader>gr', get_picker('git_branches'), opts)
 
 -- File browser
 local function file_browser()
+  return telescope.extensions.file_browser.file_browser(enter_normal_mode)
+end
+
+local function file_browser_cwd()
   return telescope.extensions.file_browser.file_browser({
-    on_complete = { function() vim.cmd('stopinsert') end }
+    cwd = vim.fn.expand('%:p:h'),
+    on_complete = { function() vim.cmd('stopinsert') end },
   })
 end
 
 map('n', '<C-n>', file_browser, opts)
+map('n', '<C-e>', file_browser_cwd, opts)
 
 telescope.setup {
   defaults = {
