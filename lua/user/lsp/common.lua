44123a8ec5c5e47aaa6a248e7e09cc2a2c046830
kolyamba2105 <haravetskyy@gmail.com>
2022-12-09 23:14:53 +0200

    LSP + telescope - Update some mappings to open picker in normal mode

--- lua/user/lsp/common.lua
+++ lua/user/lsp/common.lua
@@ -1,4 +1,4 @@
-local get_picker = require('user.telescope')
+local telescope = require('user.telescope')
 
 local M = {}
 
@@ -31,15 +31,15 @@ M.on_attach = function(client, bufnr)
   M.buf_set_keymap('[g', vim.diagnostic.goto_prev)
   M.buf_set_keymap('<leader>k', vim.lsp.buf.hover)
   M.buf_set_keymap('<leader>la', vim.lsp.buf.code_action)
-  M.buf_set_keymap('<leader>ld', get_picker('lsp_definitions'))
+  M.buf_set_keymap('<leader>ld', telescope.get_picker_normal('lsp_definitions'))
   M.buf_set_keymap('<leader>le', vim.diagnostic.open_float)
-  M.buf_set_keymap('<leader>ll', get_picker('diagnostics'))
+  M.buf_set_keymap('<leader>ll', telescope.get_picker_normal('diagnostics'))
   M.buf_set_keymap('<leader>ln', vim.lsp.buf.rename)
   M.buf_set_keymap('<leader>lq', vim.diagnostic.setloclist)
-  M.buf_set_keymap('<leader>lr', get_picker('lsp_references'))
-  M.buf_set_keymap('<leader>ls', get_picker('lsp_document_symbols'))
-  M.buf_set_keymap('<leader>lt', get_picker('lsp_type_definitions'))
-  M.buf_set_keymap('<leader>lw', get_picker('lsp_dynamic_workspace_symbols'))
+  M.buf_set_keymap('<leader>lr', telescope.get_picker_normal('lsp_references'))
+  M.buf_set_keymap('<leader>ls', telescope.get_picker('lsp_document_symbols'))
+  M.buf_set_keymap('<leader>lt', telescope.get_picker_normal('lsp_type_definitions'))
+  M.buf_set_keymap('<leader>lw', telescope.get_picker('lsp_dynamic_workspace_symbols'))
 end
 
 M.capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,9 +1,11 @@
 local telescope = require('telescope')
 local map = require('mappings')
 
+local M = {}
+
 local opts = { silent = true }
 
-local function get_picker(picker)
+M.get_picker = function(picker)
   return function(options)
     return require('telescope.builtin')[picker](require('telescope.themes').get_ivy(options))
   end
@@ -13,26 +15,26 @@ local enter_normal_mode = {
   initial_mode = 'normal',
 }
 
-local function get_picker_normal(picker)
-  return function() get_picker(picker)(enter_normal_mode) end
+M.get_picker_normal = function(picker)
+  return function() M.get_picker(picker)(enter_normal_mode) end
 end
 
 map('n', '<leader>ta', require('telescope.builtin').resume, opts)
-map('n', '<leader>tb', get_picker_normal('buffers'), opts)
-map('n', '<leader>tc', get_picker('command_history'), opts)
-map('n', '<leader>tf', get_picker('find_files'), opts)
-map('n', '<leader>tg', get_picker('live_grep'), opts)
-map('n', '<leader>th', get_picker('help_tags'), opts)
-map('n', '<leader>tj', get_picker_normal('jumplist'), opts)
-map('n', '<leader>tm', get_picker('marks'), opts)
-map('n', '<leader>tr', get_picker_normal('grep_string'), opts)
-map('n', '<leader>ts', get_picker('search_history'), opts)
-map('n', '<leader>tq', get_picker_normal('quickfix'), opts)
+map('n', '<leader>tb', M.get_picker_normal('buffers'), opts)
+map('n', '<leader>tc', M.get_picker('command_history'), opts)
+map('n', '<leader>tf', M.get_picker('find_files'), opts)
+map('n', '<leader>tg', M.get_picker('live_grep'), opts)
+map('n', '<leader>th', M.get_picker('help_tags'), opts)
+map('n', '<leader>tj', M.get_picker_normal('jumplist'), opts)
+map('n', '<leader>tm', M.get_picker('marks'), opts)
+map('n', '<leader>tr', M.get_picker_normal('grep_string'), opts)
+map('n', '<leader>ts', M.get_picker('search_history'), opts)
+map('n', '<leader>tq', M.get_picker_normal('quickfix'), opts)
 
 -- Git
-map('n', '<leader>gc', get_picker('git_commits'), opts)
-map('n', '<leader>gs', get_picker_normal('git_status'), opts)
-map('n', '<leader>gr', get_picker_normal('git_branches'), opts)
+map('n', '<leader>gc', M.get_picker('git_commits'), opts)
+map('n', '<leader>gs', M.get_picker_normal('git_status'), opts)
+map('n', '<leader>gr', M.get_picker_normal('git_branches'), opts)
 
 -- File browser
 local function file_browser_cwd()
@@ -80,4 +82,4 @@ telescope.setup {
 telescope.load_extension('file_browser')
 telescope.load_extension('ui-select')
 
-return get_picker
+return M;
