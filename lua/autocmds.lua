b80669d1ea02870819ffc0e04b333fd961fe3fa3
kolyamba2105 <haravetskyy@gmail.com>
2022-12-10 23:27:28 +0200

    Set TypeScript file type options

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,8 +1,20 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', { clear = false }),
-  desc = 'Hightlight selection on yank',
-  pattern = '*',
   callback = function()
     vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100 }
   end,
+  desc = 'Hightlight selection on yank',
+  group = vim.api.nvim_create_augroup('HighlightYank', { clear = false }),
+  pattern = '*',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    vim.cmd('compiler tsc | setlocal makeprg=yarn\\ tsc')
+  end,
+  group = vim.api.nvim_create_augroup('TypeScriptOptions', { clear = false }),
+  pattern = {
+    'typescript',
+    'typescript.tsx',
+    'typescriptreact',
+  },
 })
