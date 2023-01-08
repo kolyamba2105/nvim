b35cd79c36e78dd9ab1e79d08b99429a42b542c2
kolyamba2105 <haravetskyy@gmail.com>
2023-01-08 02:52:18 +0200

    indent-blankline - Remove catppuccin highlights

--- lua/user/indent-blankline.lua
+++ lua/user/indent-blankline.lua
@@ -4,22 +4,7 @@ vim.opt.listchars = {
   eol = "",
 }
 
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = "#f5c2e7", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = "#94e2d5", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = "#cba6f7", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = "#89dceb", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = "#f38ba8", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = "#74c7ec", nocombine = true })
-
 require('indent_blankline').setup {
   show_end_of_line = true,
   space_char_blankline = " ",
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
 }
