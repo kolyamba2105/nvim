f9d42bebe1bd029e1fc9c4b2ac5c51b0b2264492
kolyamba2105 <haravetskyy@gmail.com>
2023-01-15 16:38:57 +0200

    LSP - Change EslintFixAll mapping

--- lua/user/lsp/eslint.lua
+++ lua/user/lsp/eslint.lua
@@ -4,6 +4,6 @@ return {
   capabilities = common.capabilities,
   on_attach = function(client, bufnr)
     common.on_attach(client, bufnr)
-    common.buf_set_keymap('<C-f>', function() vim.cmd('EslintFixAll') end)
+    common.buf_set_keymap('<leader>lf', function() vim.cmd('EslintFixAll') end)
   end,
 }
