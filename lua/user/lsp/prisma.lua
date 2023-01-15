701ed2968938a32467e5eb4fc62efbda1adbc7f4
kolyamba2105 <haravetskyy@gmail.com>
2023-01-15 16:38:42 +0200

    LSP - Remove format mappings

--- lua/user/lsp/prisma.lua
+++ lua/user/lsp/prisma.lua
@@ -4,8 +4,6 @@ return {
   capabilities = common.capabilities,
   on_attach = function(client, bufnr)
     common.on_attach(client, bufnr)
-    common.buf_set_keymap('<leader>lf', function() vim.lsp.buf.format({ async = true }) end)
-
     vim.api.nvim_create_autocmd('BufWritePre', {
       callback = function() vim.lsp.buf.format() end,
       group = vim.api.nvim_create_augroup('PrismaFormat', { clear = true }),
