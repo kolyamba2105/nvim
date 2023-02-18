bf6b1e2e7c95ba94a0ebbc38c19ea28a9f6098c2
kolyamba2105 <haravetskyy@gmail.com>
2023-02-19 00:34:09 +0200

    Migrate to lazy.nvim

--- lua/user/plugins-setup.lua
+++ lua/user/plugins-setup.lua
@@ -1,54 +1,46 @@
-local map = require("user.core.mappings")
+local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-map("n", "<C-p>", "<cmd>PackerSync<cr>")
-
-local is_packer_installed = function()
-  local path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-  if vim.fn.empty(vim.fn.glob(path)) > 0 then
-    vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", path })
-    vim.cmd("packadd packer.nvim")
-
-    return true
-  end
-
-  return false
+if not vim.loop.fs_stat(path) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    path,
+  })
 end
 
-local is_installed = is_packer_installed()
+vim.opt.rtp:prepend(path)
 
-return require("packer").startup(function(use)
-  use({ "wbthomason/packer.nvim" })
-
-  use({ "catppuccin/nvim", as = "catppuccin", config = function() require("user.plugins.catppuccin") end })
-  use({ "L3MON4D3/LuaSnip" })
-  use({ "echasnovski/mini.nvim", config = function() require("user.plugins.mini") end })
-  use({ "hrsh7th/cmp-buffer" })
-  use({ "hrsh7th/cmp-nvim-lsp" })
-  use({ "hrsh7th/cmp-path" })
-  use({ "hrsh7th/nvim-cmp", config = function() require("user.plugins.cmp") end })
-  use({ "jose-elias-alvarez/typescript.nvim" })
-  use({ "kyazdani42/nvim-web-devicons" })
-  use({ "lewis6991/gitsigns.nvim", config = function() require("user.plugins.gitsigns") end })
-  use({ "lukas-reineke/indent-blankline.nvim", config = function() require("user.plugins.indent-blankline") end })
-  use({ "neovim/nvim-lspconfig", config = function() require("user.plugins.lsp") end })
-  use({ "nvim-lua/plenary.nvim" })
-  use({ "nvim-lua/popup.nvim" })
-  use({ "nvim-lualine/lualine.nvim", config = function() require("user.plugins.lualine") end })
-  use({ "nvim-telescope/telescope-file-browser.nvim" })
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-  use({ "nvim-telescope/telescope-ui-select.nvim" })
-  use({ "nvim-telescope/telescope.nvim", config = function() require("user.plugins.telescope") end })
-  use({
+require("lazy").setup({
+  { "catppuccin/nvim", as = "catppuccin", config = function() require("user.plugins.catppuccin") end },
+  { "L3MON4D3/LuaSnip" },
+  { "echasnovski/mini.nvim", config = function() require("user.plugins.mini") end },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/nvim-cmp", config = function() require("user.plugins.cmp") end },
+  { "jose-elias-alvarez/typescript.nvim" },
+  { "kyazdani42/nvim-web-devicons" },
+  { "lewis6991/gitsigns.nvim", config = function() require("user.plugins.gitsigns") end },
+  { "lukas-reineke/indent-blankline.nvim", config = function() require("user.plugins.indent-blankline") end },
+  { "neovim/nvim-lspconfig", config = function() require("user.plugins.lsp") end },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-lua/popup.nvim" },
+  { "nvim-lualine/lualine.nvim", config = function() require("user.plugins.lualine") end },
+  { "nvim-telescope/telescope-file-browser.nvim" },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  { "nvim-telescope/telescope-ui-select.nvim" },
+  { "nvim-telescope/telescope.nvim", config = function() require("user.plugins.telescope") end },
+  {
     "nvim-treesitter/nvim-treesitter",
     config = function() require("user.plugins.treesitter") end,
-    run = ":TSUpdate",
-  })
-  use({ "sitiom/nvim-numbertoggle" })
-  use({ "tpope/vim-fugitive", config = function() require("user.plugins.fugitive") end })
-  use({ "tpope/vim-repeat" })
-  use({ "tpope/vim-surround" })
-  use({ "tpope/vim-unimpaired" })
-
-  if is_installed then require("packer").sync() end
-end)
+    build = ":TSUpdate",
+  },
+  { "sitiom/nvim-numbertoggle" },
+  { "tpope/vim-fugitive", config = function() require("user.plugins.fugitive") end },
+  { "tpope/vim-repeat" },
+  { "tpope/vim-surround" },
+  { "tpope/vim-unimpaired" },
+})
