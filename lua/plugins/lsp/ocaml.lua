e901385e4afd3811e01a3b73512678eb173e6d45
kolyamba2105 <haravetskyy@gmail.com>
2023-02-28 23:25:38 +0200

    LSP - Add Format command for OCaml files

--- lua/plugins/lsp/ocaml.lua
+++ lua/plugins/lsp/ocaml.lua
@@ -5,5 +5,6 @@ return {
   on_attach = function(client, bufnr)
     common.on_attach(client, bufnr)
     common.buf_set_keymap("<leader>lf", vim.lsp.buf.format)
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function() vim.lsp.buf.format({ async = true }) end, {})
   end,
 }
