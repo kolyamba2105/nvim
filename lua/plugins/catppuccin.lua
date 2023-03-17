e78e1f68656c9c86fe0b3bdbe14abc33894efb04
kolyamba2105 <haravetskyy@gmail.com>
2023-03-17 23:38:16 +0200

    Update catppuccin highlight groups

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -8,17 +8,26 @@ require("catppuccin").setup({
 
 vim.cmd("colorscheme catppuccin")
 
-local function remove_italic(hl)
-  vim.api.nvim_set_hl(0, hl, vim.tbl_extend("force", vim.api.nvim_get_hl_by_name(hl, {}), { italic = false }))
+local groups = {
+  bold = {
+    "@keyword.export",
+  },
+  italic = {
+    "@conditional",
+    "@namespace",
+    "@parameter",
+    "@tag.attribute",
+    "@tag.attribute.tsx",
+    "@text.emphasis",
+  },
+}
+
+local function update(hl, options)
+  vim.api.nvim_set_hl(0, hl, vim.tbl_extend("force", vim.api.nvim_get_hl_by_name(hl, {}), options))
 end
 
-for _, hl in pairs({
-  "@conditional",
-  "@namespace",
-  "@parameter",
-  "@tag.attribute",
-  "@tag.attribute.tsx",
-  "@text.emphasis",
-}) do
-  remove_italic(hl)
+for setting, hls in pairs(groups) do
+  for _, hl in pairs(hls) do
+    update(hl, { [setting] = false })
+  end
 end
