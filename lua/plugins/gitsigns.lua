1e561bde3b8d4773197dc299eff8dc1658380ad3
kolyamba2105 <haravetskyy@gmail.com>
2023-06-07 13:33:03 +0300

    gitsigns - Add stage_hunk mapping

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -40,12 +40,13 @@ return {
         local map = require("core.mappings")
         local opts = { silent = true }
 
+        map("n", "[h", "<cmd>Gitsigns prev_hunk<cr>", opts)
+        map("n", "]h", "<cmd>Gitsigns next_hunk<cr>", opts)
         map("n", "<leader>b", "<cmd>Gitsigns blame_line<cr>", opts)
         map("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", opts)
         map("n", "<leader>gq", "<cmd>Gitsigns setqflist<cr>", opts)
-        map("n", "[h", "<cmd>Gitsigns prev_hunk<cr>", opts)
-        map("n", "]h", "<cmd>Gitsigns next_hunk<cr>", opts)
         map("v", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", opts)
+        map("v", "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", opts)
     end,
     event = "BufRead",
 }
