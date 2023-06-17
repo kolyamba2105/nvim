fd0f779db8cd301ff4b8a1dfe3ac861a32de8de5
kolyamba2105 <haravetskyy@gmail.com>
2023-06-17 18:14:23 +0300

    Adjust dynamic-color-column

--- lua/core/dynamic-color-column.lua
+++ lua/core/dynamic-color-column.lua
@@ -1,27 +1,27 @@
 local group = require("core.autocmds")
 
-local function set_color_column(value) vim.cmd("setlocal colorcolumn=" .. value) end
-
-local prettier_fts = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascript.jsx",
-    "javascriptreact",
-    "json",
-    "jsonc",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescript.tsx",
-    "typescriptreact",
-    "yaml",
-}
-
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function() set_color_column(vim.fn.system("prettier-print-width")) end,
+    callback = function()
+        local value = tonumber(vim.fn.system("prettier-print-width"))
+
+        if value ~= nil then vim.cmd("setlocal colorcolumn=" .. value) end
+    end,
     desc = "Set colorcolumn based on Prettier printWidth",
     group = group("PettierPrintWidthColorColumn"),
-    pattern = prettier_fts,
+    pattern = {
+        "css",
+        "graphql",
+        "html",
+        "javascript",
+        "javascript.jsx",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "markdown",
+        "scss",
+        "typescript",
+        "typescript.tsx",
+        "typescriptreact",
+        "yaml",
+    },
 })
