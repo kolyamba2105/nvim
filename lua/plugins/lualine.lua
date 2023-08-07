576e5605cacf3ed8192d2ae9fd6ab9af161c3da4
kolyamba2105 <haravetskyy@gmail.com>
2023-08-07 13:31:06 +0300

    Lualine - Set icons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,10 @@ return {
     },
     opts = {
         options = {
-            component_separators = { left = "", right = "" },
+            component_separators = {
+                left = nil,
+                right = nil,
+            },
             globalstatus = true,
             refresh = {
                 statusline = 500,
@@ -14,12 +17,15 @@ return {
             theme = "catppuccin",
         },
         sections = {
+            lualine_a = {
+                { "mode", fmt = string.lower, icon = "" },
+            },
             lualine_b = {
-                { "branch" },
+                { "branch", icon = "" },
                 { "diff" },
             },
             lualine_c = {
-                { "filename" },
+                { "filename", icon = "", path = 3 },
                 { "diagnostics", sources = { "nvim_diagnostic" } },
             },
             lualine_x = {},
