7910de577c65003038d6ab5e7ef8001679d4857d
kolyamba2105 <haravetskyy@gmail.com>
2024-01-17 13:31:50 +0200

    Adjust cmds

--- lua/core/cmds.lua
+++ lua/core/cmds.lua
@@ -1,31 +1,35 @@
+local function command(name, fn) return vim.api.nvim_create_user_command(name, fn, {}) end
+
 local function get_file_name() return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":." .. vim.fn.getcwd()) end
 
 local function get_test_name() return string.match(vim.api.nvim_get_current_line(), "[test|it]%(['\"]([^'\"]*)['\"]") end
 
-vim.api.nvim_create_user_command("CopyBufferPath", function()
+command("CopyBufferPath", function()
     local file_name = get_file_name()
 
     vim.fn.setreg("+", file_name)
     print(string.format("Copied: %s", file_name))
-end, {})
+end)
 
-vim.api.nvim_create_user_command("CopyTestCommand", function()
-    local command = string.format("yarn jest %s", get_file_name())
+command("CopyTestCommand", function()
+    local cmd = string.format("yarn jest %s", get_file_name())
 
-    vim.fn.setreg("+", command)
-    print(string.format("Copied: %s", command))
-end, {})
+    vim.fn.setreg("+", cmd)
+    print(string.format("Copied: %s", cmd))
+end)
 
-vim.api.nvim_create_user_command("CopyTestCaseCommand", function()
+command("CopyTestCaseCommand", function()
     local file_name = get_file_name()
     local test_name = get_test_name()
 
     if test_name == nil then
         print("Unable to resolve test name!")
     else
-        local command = string.format('yarn jest %s -t "%s"', file_name, test_name)
+        local cmd = string.format('yarn jest %s -t "%s"', file_name, test_name)
 
-        vim.fn.setreg("+", command)
-        print(string.format("Copied: %s", command))
+        vim.fn.setreg("+", cmd)
+        print(string.format("Copied: %s", cmd))
     end
-end, {})
+end)
+
+return command
