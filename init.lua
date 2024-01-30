ec3ee075b3b20a32eceb3d3666149bccee9045db
kolyamba2105 <haravetskyy@gmail.com>
2024-01-30 14:05:11 +0200

    Add luarocks setup

--- init.lua
+++ init.lua
@@ -1,5 +1,19 @@
 require("core")
 
+local function setup_luarocks()
+    -- stylua: ignore start
+    local path          = "/opt/homebrew/Cellar/luarocks/3.9.2/share/lua/5.4/?.lua;/opt/homebrew/share/lua/5.4/?.lua;/opt/homebrew/share/lua/5.4/?/init.lua;/opt/homebrew/lib/lua/5.4/?.lua;/opt/homebrew/lib/lua/5.4/?/init.lua;./?.lua;./?/init.lua;/Users/mykola/.luarocks/share/lua/5.4/?.lua;/Users/mykola/.luarocks/share/lua/5.4/?/init.lua"
+    local cpath         = "/opt/homebrew/lib/lua/5.4/?.so;/opt/homebrew/lib/lua/5.4/loadall.so;./?.so;/Users/mykola/.luarocks/lib/lua/5.4/?.so"
+
+    vim.o.runtimepath   = string.format("%s,%s", vim.o.runtimepath, path)
+
+    package.path        = string.format("%s;%s", package.path, path)
+    package.cpath       = string.format("%s;%s", package.cpath, cpath)
+    -- stylua: ignore end
+end
+
+setup_luarocks()
+
 local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(path) then
