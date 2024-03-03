8d918f4b5bbfefb05234a91c7d7c81a771a9c4d0
kolyamba2105 <haravetskyy@gmail.com>
2024-03-03 17:21:06 +0200

    Add dropbar.nvim config

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -4,5 +4,12 @@ return {
         dependencies = {
             "nvim-telescope/telescope-fzf-native.nvim",
         },
+        opts = {
+            bar = {
+                padding = {
+                    left = 6,
+                },
+            },
+        },
     },
 }
