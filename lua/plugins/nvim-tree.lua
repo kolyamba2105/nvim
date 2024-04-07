ece1788735bbabcd359b147e6b8c08b22598aaca
kolyamba2105 <haravetskyy@gmail.com>
2024-04-08 00:11:34 +0300

    Revert "Remove nvim-tree"
    This reverts commit 1ca3ff365b5cf3f58de1d6870032aa7ef37aa5df.

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,61 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+        local function make_view()
+            local ratio = { width = 0.25, height = 0.8 }
+
+            return {
+                float = {
+                    enable = false,
+                    open_win_config = function()
+                        local screen = {
+                            width = vim.opt.columns:get(),
+                            height = vim.opt.lines:get() - vim.opt.cmdheight:get(),
+                        }
+                        local window = {
+                            width = screen.width * ratio.width,
+                            height = screen.height * ratio.height,
+                        }
+
+                        local width = math.floor(window.width)
+                        local height = math.floor(window.height)
+
+                        local col = (screen.width - window.width) / 2
+                        local row = ((vim.opt.lines:get() - window.height) / 2) - vim.opt.cmdheight:get()
+
+                        return {
+                            border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+                            col = col,
+                            height = height,
+                            relative = "editor",
+                            row = row,
+                            width = width,
+                        }
+                    end,
+                },
+                width = function() return math.floor(vim.opt.columns:get() * ratio.width) end,
+            }
+        end
+
+        require("nvim-tree").setup({
+            disable_netrw = true,
+            git = { ignore = false },
+            hijack_cursor = true,
+            modified = {
+                enable = true,
+            },
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                },
+            },
+            view = make_view(),
+        })
+
+        vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<cr>", {
+            desc = "Open nvim-tree",
+            silent = true,
+        })
+    end,
+    enabled = false,
+}
