c4c078f4415caac739afc59d580a5f087b614c29
kolyamba2105 <haravetskyy@gmail.com>
2025-01-06 10:24:06 +0200

    Remove DadBodSplitSize autocmd

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -4,12 +4,6 @@ return {
         local group = require("core.autocmds")
 
         vim.api.nvim_create_autocmd("BufReadPost", {
-            callback = function() vim.cmd.resize(32) end,
-            desc = "Resize vim-dadbot output window",
-            group = group("DadBodSplitSize"),
-            pattern = "*.dbout",
-        })
-        vim.api.nvim_create_autocmd("BufReadPost", {
             callback = function() vim.b.minitrailspace_disable = true end,
             desc = "Disable mini.trailspace for vim-dadbod output window",
             group = group("DadBodDisableTrailSpace"),
