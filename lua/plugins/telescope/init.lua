404434d53803b475d03b860dd91d1c45cfdfbc8e
kolyamba2105 <haravetskyy@gmail.com>
2025-04-03 22:43:44 +0300

    Telescope winborder fix (temporary hack)

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -109,6 +109,18 @@ return {
 
         telescope.load_extension("fzf")
         telescope.load_extension("ui-select")
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "TelescopeFindPre",
+            callback = function()
+                vim.opt.winborder = "none"
+
+                vim.api.nvim_create_autocmd("WinLeave", {
+                    once = true,
+                    callback = function() vim.opt.winborder = "double" end,
+                })
+            end,
+        })
     end,
     dependencies = {
         { "nvim-lua/plenary.nvim" },
