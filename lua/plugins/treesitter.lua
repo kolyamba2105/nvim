49aafe645ff7802b1823eb52565b297761301364
kolyamba2105 <haravetskyy@gmail.com>
2025-05-08 11:55:44 +0300

    Disable treesitter for large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+        ---@diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.configs").setup({
             context_commentstring = {
                 enable = true,
@@ -33,6 +34,13 @@ return {
             },
             highlight = {
                 enable = true,
+                disable = function(_, buf)
+                    local max_file_size = 100 * 1024 -- 100 KB
+
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+
+                    return ok and stats and stats.size > max_file_size
+                end,
             },
             incremental_selection = {
                 enable = false,
