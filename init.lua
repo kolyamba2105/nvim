b1b4f25bfa48ac3dac31b4f9ee8b5941e29ef492
kolyamba2105 <haravetskyy@gmail.com>
2025-05-31 00:28:18 +0300

    Adjust init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,11 @@
 require("core")
 
-local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local repo = "https://github.com/folke/lazy.nvim.git"
+local path = string.format("%s/lazy/lazy.nvim", vim.fn.stdpath("data"))
 
+--- @diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(path) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        path,
-    })
+    vim.fn.system({ "git", "clone", "--branch=stable", "--filter=blob:none", repo, path })
 end
 
 vim.opt.rtp:prepend(path)
