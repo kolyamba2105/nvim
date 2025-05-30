c9ad7e2e7a76729c7770863b37d2cbf71a3a8b87
kolyamba2105 <haravetskyy@gmail.com>
2025-05-31 00:01:04 +0300

    Move plugins into a plugins.lua file

--- lua/plugins/indent-blankline.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function() require("ibl").setup({ indent = { char = "╎" }, scope = { enabled = false } }) end,
-    event = "BufRead",
-    main = "ibl",
-}
--- lua/plugins/telescope/picker.lua
+++
@@ -1,7 +0,0 @@
---- @param name string
---- @param options? { [string]: unknown }
-return function(name, options)
-    return function()
-        return require("telescope.builtin")[name](vim.tbl_extend("force", { initial_mode = "insert" }, options or {}))
-    end
-end
