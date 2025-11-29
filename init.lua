52fabb0fc95171287e435023a2cfbb1b6838f56e
kolyamba2105 <haravetskyy@gmail.com>
2025-11-29 09:27:44 +0200

    LSP - Re-add "inlayHints" settings on "vtsls"

--- init.lua
+++ init.lua
@@ -1014,6 +1014,26 @@ local plugins = {
                         },
                     },
                     javascript = {
+                        inlayHints = {
+                            enumMemberValues = {
+                                enabled = true,
+                            },
+                            functionLikeReturnTypes = {
+                                enabled = true,
+                            },
+                            parameterNames = {
+                                enabled = "literals",
+                            },
+                            parameterTypes = {
+                                enabled = true,
+                            },
+                            propertyDeclarationTypes = {
+                                enabled = true,
+                            },
+                            variableTypes = {
+                                enabled = true,
+                            },
+                        },
                         preferences = {
                             importModuleSpecifier = "non-relative",
                             importModuleSpecifierEnding = "minimal",
@@ -1021,6 +1041,23 @@ local plugins = {
                         },
                     },
                     typescript = {
+                        inlayHints = {
+                            functionLikeReturnTypes = {
+                                enabled = true,
+                            },
+                            parameterNames = {
+                                enabled = "literals",
+                            },
+                            parameterTypes = {
+                                enabled = true,
+                            },
+                            propertyDeclarationTypes = {
+                                enabled = true,
+                            },
+                            variableTypes = {
+                                enabled = true,
+                            },
+                        },
                         preferences = {
                             importModuleSpecifier = "non-relative",
                             importModuleSpecifierEnding = "minimal",
